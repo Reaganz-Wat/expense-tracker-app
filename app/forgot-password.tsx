d8b9a99b53816819f9a54e2076cd662d569cb2a1
Reaganz-Wat <reaganwatmon6@gmail.com>
2025-05-18 16:15:29 +0300

    feat: add assets-contents

---
+++ app/forgot-password.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const ForgotPassword = () => {
+  return (
+    <View>
+        <Text>
+            Forgot Password
+        </Text>
+    </View>
+  )
+}
+
+export default ForgotPassword
